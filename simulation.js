a0dfc3ca070e89bd19733d2942c194e3dfdd8520
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2020-11-07 23:40:59 -0500

    fixes

--- simulation.js
+++ simulation.js
@@ -9,6 +9,22 @@ var pT = [];
 var modal;
 
 
+function help() {
+    modal = document.getElementById("simInfo");
+    modal.style.display = "block";
+
+    document.getElementsByClassName("close")[0].onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
 function start() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('all').innerHTML = '<h1>View on a computer >:(</h1><br/><img src="linus.jpg" alt="ur bad" width="100%">'
@@ -39,18 +55,8 @@ function start() {
             pT.push(pT[0]);
         }
         draw();
-        modal = document.getElementById("simInfo");
-        modal.style.display = "block";
-
-        document.getElementsByClassName("close")[0].onclick = function () {
-            modal.style.display = "none";
-        }
-
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+        clearInterval(lifeCycle);
+        document.getElementById('playPause').innerHTML = 'PLAY'
     }
 }
 
